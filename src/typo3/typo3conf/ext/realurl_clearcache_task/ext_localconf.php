5a1e0d9733c5833ce8acf51db1992b6655a29cb3
Dan Untenzu <untenzu@webit.de>
2016-07-28 17:37:59 +0200

    [FEATURE] Remove support for TYPO3 < 6
    Drop old version support

--- src/typo3/typo3conf/ext/realurl_clearcache_task/ext_localconf.php
+++ src/typo3/typo3conf/ext/realurl_clearcache_task/ext_localconf.php
@@ -9,59 +9,29 @@ $GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['Webit\\RealurlC
 		'description' => 'Task to delete old entries from specific tables of RealUrl.',
 		'additionalFields' => 'Webit\\RealurlClearcacheTask\\Task\\MultipleTablesGarbageCollectionAdditionalFieldProvider'
 );
-$version = class_exists('t3lib_utility_VersionNumber')
-	? t3lib_utility_VersionNumber::convertVersionNumberToInteger(TYPO3_version)
-	: t3lib_div::int_from_ver(TYPO3_version);
-if ($version >= 6000000) {
-	// Register realurl cache-tables in table garbage collection task
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_chashcache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_chashcache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_pathcache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_pathcache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urldecodecache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urldecodecache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urlencodecache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urlencodecache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
-} else {
-	// …same for TYPO3 CMS < 6.0
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_chashcache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_chashcache'] = array(
+
+// Register realurl cache-tables in table garbage collection task
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_chashcache'])) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_chashcache'] = array(
 			'dateField' => 'tstamp',
 			'expirePeriod' => 180
-		);
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_pathcache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_pathcache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_urldecodecache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_urldecodecache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
-	if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_urlencodecache'])) {
-		$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['tx_scheduler_TableGarbageCollection']['options']['tables']['tx_realurl_urlencodecache'] = array(
-				'dateField' => 'tstamp',
-				'expirePeriod' => 180
-		);
-	}
+	);
+}
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_pathcache'])) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_pathcache'] = array(
+			'dateField' => 'tstamp',
+			'expirePeriod' => 180
+	);
+}
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urldecodecache'])) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urldecodecache'] = array(
+			'dateField' => 'tstamp',
+			'expirePeriod' => 180
+	);
+}
+if (!is_array($GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urlencodecache'])) {
+	$GLOBALS['TYPO3_CONF_VARS']['SC_OPTIONS']['scheduler']['tasks']['TYPO3\\CMS\\Scheduler\\Task\\TableGarbageCollectionTask']['options']['tables']['tx_realurl_urlencodecache'] = array(
+			'dateField' => 'tstamp',
+			'expirePeriod' => 180
+	);
 }
